bede8fae5314982d995863aa56259ccb1f4ad43f
taddeimania <jtaddei@gmail.com>
2015-11-19 14:06:02 -0500

    point wsgi at production settings

--- watcher/wsgi.py
+++ watcher/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watcher.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watcher.settings.production")
 
 application = get_wsgi_application()

2c4d5603b3f40a4ae361a867a9494318b4e07331
taddeimania <jtaddei@gmail.com>
2015-12-09 20:44:43 -0500

    lol fixed stupid error

--- watcher/celery.py
+++ watcher/celery.py
@@ -1,9 +1,9 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watcher.settings.production')
 
 from django.conf import settings
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watcher.settings.production')
 app = Celery('watcher')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)

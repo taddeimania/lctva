822d850caeee9053546c6d053ddc4a8979b64aec
taddeimania <jtaddei@gmail.com>
2015-12-27 23:53:30 -0500

    Dreams of limiting days on streaming dashboard

--- app/tests.py
+++ app/tests.py
@@ -5,10 +5,6 @@ from django.test import TestCase
 from app.models import Node
 from app.utils import daily_aggregator
 
-# Create your tests here.
-
-# woops - no tests yet
-
 
 class DailyBreakdownTestCase(TestCase):
 
@@ -17,6 +13,8 @@ class DailyBreakdownTestCase(TestCase):
         self.day_two = datetime.datetime(year=2015, month=11, day=26)
         self.day_three = datetime.datetime(year=2015, month=11, day=27)
         self.day_four = datetime.datetime(year=2015, month=11, day=28)
+        self.day_five = datetime.datetime(year=2015, month=11, day=29)
+        self.day_six = datetime.datetime(year=2015, month=11, day=30)
 
     def create_node(self, timestamp, total=0):
         node = Node.objects.create(
@@ -59,3 +57,15 @@ class DailyBreakdownTestCase(TestCase):
         self.assertEqual(aggregate_total[1].mean, 2.5)
         self.assertEqual(aggregate_total[1].max, 6)
         self.assertEqual(aggregate_total[1].day, '2015-11-25')
+
+    # def test_daily_aggregator_only_returns_5_days_starting_with_most_recent_day(self):
+    #     self.create_node(self.day_one, total=6)
+    #     self.create_node(self.day_two, total=1)
+    #     self.create_node(self.day_three, total=3)
+    #     self.create_node(self.day_four, total=0)
+    #     self.create_node(self.day_five, total=9)
+    #     self.create_node(self.day_six, total=10)
+    #     qs = Node.objects.filter(livetvusername="taddeimania")
+    #     aggregate_total = daily_aggregator(qs)
+    #     self.assertEqual(len(aggregate_total), 5)
+    #     self.assertEqual(aggregate_total[0].day, '2015-11-30')

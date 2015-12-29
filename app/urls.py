f4a8417c5bd46c7fc7a9632b8fdac7efbf9926d3
taddeimania <jtaddei@gmail.com>
2015-12-28 22:42:09 -0500

    Added login required decorator to history detail view

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index_view"),
     url(r'^dashboard/$', login_required(DashboardView.as_view()), name="dashboard_view"),
     # url(r'^history/$', ),
-    url(r'^history/(?P<datestamp>\d{4}-\d{2}-\d{2})/$', HistoryDetailView.as_view(), name="history_detail_view"),
+    url(r'^history/(?P<datestamp>\d{4}-\d{2}-\d{2})/$', login_required(HistoryDetailView.as_view()), name="history_detail_view"),
     url(r'^extra/$', login_required(ExtraView.as_view()), name="extra_view"),
     url(r'^api/graph/$', GraphView.as_view(), name="graph_view"),
     url(r'^api/graph/friends/$', FriendsGraphView.as_view(), name="friends_graph_view"),

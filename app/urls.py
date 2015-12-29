e7716150239d3b0e6f6ae81b605d31d4e6823d2d
taddeimania <jtaddei@gmail.com>
2015-12-28 21:05:57 -0500

    Cleanup dead code and reword ui

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from app.views import IndexView, DashboardView, StreamActivateView, StreamDeactivateView, GraphView, FriendsGraphView, ExtraView
+from app.views import IndexView, DashboardView, GraphView, FriendsGraphView, ExtraView
 
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index_view"),
     url(r'^dashboard/$', login_required(DashboardView.as_view()), name="dashboard_view"),
     url(r'^extra/$', login_required(ExtraView.as_view()), name="extra_view"),
-    url(r'^stream_activate/$', login_required(StreamActivateView.as_view()), name="stream_activate"),
-    url(r'^stream_deactivate/$', login_required(StreamDeactivateView.as_view()), name="stream_deactivate"),
     url(r'^api/graph/$', GraphView.as_view(), name="graph_view"),
     url(r'^api/graph/friends/$', FriendsGraphView.as_view(), name="friends_graph_view"),
 ]

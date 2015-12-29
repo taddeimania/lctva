b3b7592548561d6288880257551722a2f7a93ffb
taddeimania <jtaddei@gmail.com>
2015-12-29 15:14:32 -0500

    Added About page

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from app.views import IndexView, DashboardView, GraphView, FriendsGraphView, ExtraView, HistoryDetailView
+from app.views import IndexView, AboutView, DashboardView, GraphView, FriendsGraphView, ExtraView, HistoryDetailView
 
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index_view"),
+    url(r'^about/$', AboutView.as_view(), name="about_view"),
     url(r'^dashboard/$', login_required(DashboardView.as_view()), name="dashboard_view"),
     # url(r'^history/$', ),
     url(r'^history/(?P<datestamp>\d{4}-\d{2}-\d{2})/$', login_required(HistoryDetailView.as_view()), name="history_detail_view"),
--- app/views.py
+++ app/views.py
@@ -18,6 +18,15 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
 
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["is_joel_live"] = UserProfile.objects.get(livetvusername="taddeimania").active
+        return context
+
+
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
 

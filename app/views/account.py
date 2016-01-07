3b4a2bc81decbea7c0fc24eec64c2bb98782085e
taddeimania <jtaddei@gmail.com>
2016-01-07 14:28:57 -0500

    fancying up notification page

--- app/views/account.py
+++ app/views/account.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import TemplateView, View
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
@@ -42,3 +43,8 @@ class NotificationListView(ListView):
         for notification in qs:
             notification.readers.add(self.request.user)
         return qs
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["current_time"] = timezone.now()
+        return context

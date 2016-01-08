e7cdab08d4834949e04b6bfdd737764fa8af6360
taddeimania <jtaddei@gmail.com>
2016-01-08 13:16:20 -0500

    Get leaders from model created by task

--- app/views/leaderboard.py
+++ app/views/leaderboard.py
@@ -1,5 +1,8 @@
+import datetime
 
 from django.views.generic import TemplateView
+
+from app.models import Leaderboard
 
 
 class LeaderBoardDailyView(TemplateView):
@@ -7,4 +10,7 @@ class LeaderBoardDailyView(TemplateView):
 
     def get_context_data(self):
         context = super().get_context_data()
+        leaderboard = Leaderboard.objects.filter(date=datetime.date.today() + datetime.timedelta(days=-1))
+        if leaderboard:
+            context["leaderboard_data"] = leaderboard.get()
         return context

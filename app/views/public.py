467717cf80dde140624341744cc9a9ece0fb3658
taddeimania <jtaddei@gmail.com>
2016-01-08 16:37:21 -0500

    removed active flag on userprofile and left this in the about page, woops

--- app/views/public.py
+++ app/views/public.py
@@ -1,8 +1,6 @@
 
 from django.views.generic import TemplateView
 
-from app.models import UserProfile
-
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -10,8 +8,3 @@ class IndexView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = "about.html"
-
-    def get_context_data(self):
-        context = super().get_context_data()
-        context["is_joel_live"] = UserProfile.objects.get(livetvusername="taddeimania").active
-        return context
